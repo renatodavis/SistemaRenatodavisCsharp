6e54d7d5c5d02752f8d49e757e93ceef457ad34e
Renato Davis <renatodavis@gmail.com>
2022-01-23 17:53:47 -0300

    Projeto c# EF Cores

---
+++ renatodavis.app.domain/entities/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace renatodavis.app.domain.entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
---
+++ renatodavis.app.domain/entities/Fornecedor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace renatodavis.app.domain.entities
+{
+    public class Fornecedor
+
+    {
+        public int FornecedorId { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
