9b139009f39643576843467a6a6fd56581b49a70
Renato Davis <renatodavis@gmail.com>
2022-01-23 21:01:34 -0300

    Add

---
+++ renatodavis.app.infra/Contexto/AppContexto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using renatodavis.app.domain.entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace renatodavis.app.infra.Contexto
+{
+    public class AppContexto : DbContext
+    {
+        public AppContexto(DbContextOptions<AppContexto> options  ):base(options)
+        {
+
+        }
+        
+        // Mapeamento das entidades
+        public DbSet<Cliente> Clientes { get; set; }
+
+        // configurações do EF 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
