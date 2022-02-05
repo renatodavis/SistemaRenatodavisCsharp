8543ce7bb686a91775b66b06abb5845767a28552
renatodavis <renatodavis@hotmail.com>
2022-02-05 18:43:01 -0300

    Incluído InterfaceProduto, repositório produto

---
+++ renatodavis.app.infra/repositores/ProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿using renatodavis.app.domain.entities;
+using renatodavis.app.domain.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace renatodavis.app.infra.repositores
+{
+    public class ProdutoRepository : RepositoryBase<Produto>, IProdutoRepository
+    {
+        public bool bExisteProduto(int id)
+        {
+            return Db.Set<Produto>().Any(e => e.ProdutoId == id);
+        }
+    }
+}
